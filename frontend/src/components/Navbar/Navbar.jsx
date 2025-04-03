9947b1c6294c915a3c2e0173c7d095f205020dd8
ikarannarode <karannarode999@gmail.com>
2025-04-03 19:23:12 +0530

    Add conditional rendering for cart icon in Navbar based on user authentication

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -26,10 +26,10 @@ navigate("/")
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
-                <div className="navbar-search-icon">
-                    <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                    <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
-                </div>
+               {token? <div className="navbar-search-icon">
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+                </div>:""}
                 {!token?
                 <button onClick={() => setShowLogin(true)}>Sign in</button>:
                 <div className="navbar-profile">
